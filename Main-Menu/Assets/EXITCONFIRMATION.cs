93995168df8106a2309fd4b2a10c08f73961be19
Rochak498 <rochakkhadka450@gmail.com>
2025-04-14 14:56:12 +0800

    exit confirmation panel
    changes

---
+++ Main-Menu/Assets/EXITCONFIRMATION.cs
@@ -0,0 +1,11 @@
+
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EXITCONFIRMATION : MonoBehaviour
+{
+    public void LoadExit()
+    {
+        SceneManager.LoadScene("EXITCONFIRMATION");
+    }
+}
