8b4b068b8e166266817af4adcf0d15d0d7c05439
Rochak498 <rochakkhadka450@gmail.com>
2025-04-15 00:37:47 +0800

    changed last time to add removed code from level scene and I added back
    gg

---
+++ Main-Menu/Assets/Rochak's Scripts/roaminghero.cs
@@ -0,0 +1,10 @@
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class roaminghero : MonoBehaviour
+{
+   public void loadRoamingHero()
+   {
+    SceneManager.LoadScene("RoamingHero");
+   }
+}
