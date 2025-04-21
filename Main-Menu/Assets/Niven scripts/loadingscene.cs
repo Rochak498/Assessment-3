d5de34c535bd60d8055a92c2aef6451c5d5beaea
Niven215 <nivenmugambi@gmail.com>
2025-04-21 12:12:10 +0800

    connecting game

--- Main-Menu/Assets/Niven scripts/loadingscene.cs
+++ Main-Menu/Assets/Niven scripts/loadingscene.cs
@@ -3,11 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class loadingscene : MonoBehaviour
+public class loadingscene: MonoBehaviour
 {
-    public void LoadMainLevel()
+    public void LoadMainScene()
     {
-        SceneManager.LoadScene("Levels");
+        SceneManager.LoadScene("FlowerLeveling");
     }
 
     public void QuitGame()
@@ -16,8 +16,19 @@ public class loadingscene : MonoBehaviour
         Debug.Log("Quit Game"); // For editor testing
     }
 
-    public void LoadFlowerLevelingScene()
+    public void LoadEndScene()
     {
-        SceneManager.LoadScene("FlowerLeveling"); // make sure the name matches your scene name!
+        SceneManager.LoadScene("FlowerLevelingEndScene");
+    }
+
+    // Call this to go back to MainMenu
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void LoadLevel()
+    {
+        SceneManager.LoadScene("Levels");
     }
 }
